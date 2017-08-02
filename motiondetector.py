bdde3bb7bcceff227cd5909e4a40ac322a3a88a0
Andy Khov <khovandyak@gmail.com>
2017-08-01 18:48:45 -0700

    inital .py file

---
+++ motiondetector.py
@@ -0,0 +1,24 @@
+import RPi.GPIO as GPIO
+import time
+
+sensor = 16
+
+GPIO.setmode(GPIO.BOARD) #sets up pin numbering to BOARD
+GPIO.setup(sensor, GPIO.IN) #initializes channel of sensor as an input
+
+sensorState = 0
+
+try:
+    while True:
+        time.sleep(0.1)
+        sensorState = GPIO.input(sensor)
+        print(sensorState)
+        if sensorState == GPIO.HIGH:
+            sensorState = GPIO.LOW
+            time.sleep(7)
+        time.sleep(0.1)
+        
+except KeyboardInterrupt:
+    pass
+finally:
+    GPIO.cleanup();
