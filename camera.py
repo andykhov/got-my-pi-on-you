9a9496d203bf17062f4c504504c4571ddcb9f02d
Andy Khov <khovandyak@gmail.com>
2017-08-05 17:18:55 -0700

    added comments

--- camera.py
+++ camera.py
@@ -6,13 +6,16 @@ class Webcam:
     webcamCount = 0;
 
     def __init__(self, device, resolution, colorspace):
-        pygame.camera.init()
+        pygame.camera.init() #initialize the camera module
+        #load specified camera, "device"
         self.cam = pygame.camera.Camera(device, (resolution[0], resolution[1]), colorspace)
-        self.cam.start()
+        self.cam.start() #webcam opens, initializes and starts capturing
 
+    #captures an image frame and saves it to imageName
     def takePicture(self, imageName):
         self.img = self.cam.get_image()
         pygame.image.save(self.img, imageName)
 
+    #webcam stops, uninitializes, and closes
     def stopCam(self):
         self.cam.stop()
