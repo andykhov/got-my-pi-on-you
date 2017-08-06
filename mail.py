93d2643bc1544d0d996d66c225f3861c993d1890
Andy Khov <khovandyak@gmail.com>
2017-08-05 21:17:30 -0700

    added MIME image feature

--- mail.py
+++ mail.py
@@ -1,4 +1,8 @@
+import os
 import smtplib
+from email.mime.text import MIMEText
+from email.mime.image import MIMEImage
+from email.mime.multipart import MIMEMultipart
 
 class email:
 
@@ -15,7 +19,18 @@ class email:
         self.smtpconnection.starttls() #enable TLS encryption
         self.smtpconnection.login(self.address, self.password)
 
-    def sendEmail(self, recipient, message):
+    def sendEmail(self, recipient, subject, message, imgPath):
+        msg = MIMEMultipart()
+        msg["Subject"] = subject
+        msg["From"] = self.address
+        msg["To"] = recipient
+        msg.attach(MIMEText(message))
+
+        imgfp = open(imgPath, "rb")
+        img = MIMEImage(imgfp.read())
+        imgfp.close()
+        msg.attach(img)
+
         self.smtpconnection.sendmail(self.address, recipient, message)
 
     def closeSMTP(self):
