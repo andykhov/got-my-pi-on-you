612f0eeea07ee5d3ae1793154cb7b86178ade0c8
Andy Khov <khovandyak@gmail.com>
2017-08-08 16:36:42 -0700

    fixed bug: added a line to take a pic

--- src/camera.py
+++ src/camera.py
@@ -1,11 +1,15 @@
 import cv2
 import numpy
+import time
 
 def takePicture(device, imgPath):
     webcam = cv2.VideoCapture(device)
-    
-    while (webcam.isOpened() == False):
-        webcam.open(device)
 
+    while (webcam.isOpened()):
+        webcam.open(device)
+        time.sleep(1)
+
+    ret, frame = webcam.read()
     cv2.imwrite(imgPath, frame)
+
     webcam.release()
