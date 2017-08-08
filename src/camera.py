f299786f3bf2f6da5978a98298323ada2bd73a7a
Andy Khov <khovandyak@gmail.com>
2017-08-08 16:38:14 -0700

    added comments

--- src/camera.py
+++ src/camera.py
@@ -3,13 +3,14 @@ import numpy
 import time
 
 def takePicture(device, imgPath):
-    webcam = cv2.VideoCapture(device)
+    webcam = cv2.VideoCapture(device) #returns a VideoCapture obj
 
+    #poll until webcam is opened and ready
     while (webcam.isOpened()):
         webcam.open(device)
         time.sleep(1)
 
-    ret, frame = webcam.read()
-    cv2.imwrite(imgPath, frame)
+    ret, frame = webcam.read() #reads current fram from webcam
+    cv2.imwrite(imgPath, frame) #save frame to imgPath
 
     webcam.release()
